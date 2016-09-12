dc2ac8954e697b56d82cfe5ba4e7cc7ea17d2955
rachael <hobbs.rachael@gmail.com>
2016-09-12 13:07:49 -0400

    first commit

--- src/game.js
+++ src/game.js
@@ -1,6 +1,22 @@
-var Node = require('./node')
+var Node = require('./node');
 
 var Game = function() {
+  this.nodes = {};
+  this.startingPoint = null;
+}
+
+Game.prototype.addNode = function(title, text){
+  //var node = new Node(title, text);
+  if(!this.nodes.includes(title)){
+    this.nodes[title] = text;
+  } else {
+    throw new Error();
+  }
+  return ;
+  // this.title = title;
+  // this.text = text;
+  // this.connections = [];
+  // this.conditions = {};
 
 }
 
--- src/node.js
+++ src/node.js
@@ -1,6 +1,18 @@
 var Connection = require('./connection')
 
 var Node = function(title, text) {
+  this.title = title;
+  this.text = text;
+  this.connections = [];
+  this.conditions = {};
+}
+
+Node.prototype.connect = function(node, text){
+  var connection = new Connection(node, text);
+  if(!this.connections.includes(connection)){
+      this.connections.push(connection);
+      this.conditions[text] = connection;
+  }
 
 }
 
